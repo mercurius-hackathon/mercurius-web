e9bf0f1256f596d76ed6c5ac0bbe4735c6a693a4
JohnTrump <wujunchuan1994@hotmail.com>
2018-04-21 12:27:10 +0900

    show the symbol

--- src/components/AreaChart.vue
+++ src/components/AreaChart.vue
@@ -9,20 +9,13 @@
 
 <script>
 import echarts from "echarts/lib/echarts";
-// mock data
-// var date = []
-// var data = []
-// for (var i = 1; i < 60; i++) {
-//   date.push(i + 11)
-//   data.push(Math.round((Math.random()) * 10 + 10))
-// }
-var base = +new Date(1968, 9, 3);
+var base = +new Date(2008, 8, 8);
 var oneDay = 24 * 3600 * 1000;
 var date = [];
 
 var data = [Math.random() * 300];
 
-for (var i = 1; i < 20000; i++) {
+for (var i = 1; i < 2000; i++) {
   var now = new Date((base += oneDay));
   date.push([now.getFullYear(), now.getMonth() + 1, now.getDate()].join("/"));
   data.push(Math.round((Math.random() - 0.5) * 20 + data[i - 1]));
@@ -36,7 +29,7 @@ const lightThemeOptions = {
       lineStyle: {
         color: "#1863a5",
         opacity: 0.5,
-        width: 2
+        width: 1
       }
     },
     axisLabel: {
@@ -65,7 +58,6 @@ const lightThemeOptions = {
         normal: {
           type: "solid",
           color: "#2D73B0",
-          opacity: "0.1"
         }
       },
       areaStyle: {
@@ -87,6 +79,24 @@ const lightThemeOptions = {
 };
 
 const commonOptions = {
+  // zoom
+  dataZoom: [{
+    type: 'inside',
+    start: 0,
+    end: 10
+  }, {
+    start: 0,
+    end: 10,
+    handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+    handleSize: '80%',
+    handleStyle: {
+      color: '#fff',
+      shadowBlur: 3,
+      shadowColor: 'rgba(0, 0, 0, 0.6)',
+      shadowOffsetX: 2,
+      shadowOffsetY: 2
+    }
+  }],
   /** grid配置 */
   grid: {
     borderWidth: 0,
@@ -98,14 +108,17 @@ const commonOptions = {
   },
   /** tooltip配置 */
   tooltip: {
+    trigger: 'axis',
     show: true,
-    trigger: "axis",
     position: function(pt) {
       return [pt[0], "10%"];
     },
     axisPointer: {
       animation: false,
-      type: "cross"
+      type: "cross",
+      label: {
+        backgroundColor: '#6a7985'
+      }
     }
   },
   axisPointer: {
@@ -160,9 +173,8 @@ const commonOptions = {
     {
       name: "模拟数据",
       type: "line",
-      smooth: false,
-      symbol: "none",
-      sampling: "average",
+      smooth: true,
+      showSymbol: false,
       data: data
     }
   ]
--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import ECharts from "vue-echarts/components/ECharts.vue";
 // 手动引入 ECharts 各模块来减小打包体积
 import "echarts/lib/component/tooltip";
 import "echarts/lib/chart/line";
-
+import "echarts/lib/component/dataZoom"
 Vue.config.productionTip = false;
 
 // regist components
