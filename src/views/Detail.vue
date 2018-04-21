8a1119529812847853dfac27ed6e499b638e312b
JohnTrump <wujunchuan1994@hotmail.com>
2018-04-21 22:06:49 +0900

    change layout

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -5,54 +5,68 @@
 */
 <template>
     <div>
-      <el-tabs type="border-card">
-      <!-- profit chart module -->
-      <el-tab-pane label="Profit chart">
-        <div class="areachart">
-          <area-chart class="echarts"></area-chart>
-        </div>
-      </el-tab-pane>
-      <el-tab-pane label="Strategy chart">
-        <div class="mutillinechart">
-          <multi-line-chart class="echarts"></multi-line-chart>
-        </div>
-      </el-tab-pane>
-      <el-tab-pane label="Status">
-        <div class="status-wrapper">
-          <p> Time: 2018-04-21 17:45:59 </p>
-          <p> DeepTimes:1264139 </p>
-          <p> P: 17.182</p>
-          <p> M: 14.158</p>
-          <p>2.5: P_COUNT: 5 M_COUNT: 3</p>
-          <p>[{"MarginLevel":10,"Amount":2024,"FrozenAmount":0,"Price":606.1285,"Profit":0.478648,"Type":0,"ContractType":"this_week"},{"MarginLevel":10,"Amount":2024,"FrozenAmount":0,"Price":618.7885,"Profit":-0.595328,"Type":1,"ContractType":"quarter"}]</p>
-        </div>
-      </el-tab-pane>
-      <el-tab-pane label="Logs">
-        <div class="log-contents">
-          <div class="log">
-            <div class="scroll-log">
-              <!-- loop content -->
-              <div class="level1">
-                <span class="time">2018-02-28 10:30</span>
-                <span class="message">Profit: 18.1734% (15.9673% over B&H)</span>
+      <!-- row 1 -->
+      <el-row>
+        <el-col :span="24">
+          <el-tabs type="border-card">
+            <!-- profit chart module -->
+            <el-tab-pane label="Profit chart">
+              <div class="areachart">
+                <area-chart class="echarts"></area-chart>
               </div>
-              <div class="level2">
-                <span class="time">2018-02-28 10:30</span>
-                <span class="message">Stop detected.</span>
+            </el-tab-pane>
+            <el-tab-pane label="Strategy chart">
+              <div class="mutillinechart">
+                <multi-line-chart class="echarts"></multi-line-chart>
               </div>
-              <div class="level0">
-                <span class="time">2018-02-28 10:30</span>
-                <span class="message">Profit: 18.1734% (15.9673% over B&H)</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </el-tab-pane>
-    </el-tabs>
+            </el-tab-pane>
+          </el-tabs>
+        </el-col>
+      </el-row>
+      <!-- row 2 -->
+      <el-row class="row2">
+        <el-col :span="12" class="el-col">
+          <el-tabs type="border-card" class="test">
+            <el-tab-pane label="BlackTest">BlackTest</el-tab-pane>
+          </el-tabs>
+        </el-col>
 
-    <el-tabs type="border-card" class="test">
-      <el-tab-pane label="BlackTest">BlackTest</el-tab-pane>
-    </el-tabs>
+        <el-col :span="12" class="el-col">
+          <el-tabs type="border-card">
+            <el-tab-pane label="Status">
+              <div class="status-wrapper">
+                <p> Time: 2018-04-21 17:45:59 </p>
+                <p> DeepTimes:1264139 </p>
+                <p> P: 17.182</p>
+                <p> M: 14.158</p>
+                <p>2.5: P_COUNT: 5 M_COUNT: 3</p>
+                <p>[{"MarginLevel":10,"Amount":2024,"FrozenAmount":0,"Price":606.1285,"Profit":0.478648,"Type":0,"ContractType":"this_week"},{"MarginLevel":10,"Amount":2024,"FrozenAmount":0,"Price":618.7885,"Profit":-0.595328,"Type":1,"ContractType":"quarter"}]</p>
+              </div>
+            </el-tab-pane>
+            <el-tab-pane label="Logs">
+              <div class="log-contents">
+                <div class="log">
+                  <div class="scroll-log">
+                    <!-- loop content -->
+                    <div class="level1">
+                      <span class="time">2018-02-28 10:30</span>
+                      <span class="message">Profit: 18.1734% (15.9673% over B&H)</span>
+                    </div>
+                    <div class="level2">
+                      <span class="time">2018-02-28 10:30</span>
+                      <span class="message">Stop detected.</span>
+                    </div>
+                    <div class="level0">
+                      <span class="time">2018-02-28 10:30</span>
+                      <span class="message">Profit: 18.1734% (15.9673% over B&H)</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </el-tab-pane>
+          </el-tabs>
+        </el-col>
+      </el-row>
   </div>
 
 </template>
@@ -122,11 +136,10 @@ export default {
   width: 100%;
 }
 .status-wrapper  {
+  word-break: break-all;
+  font-size: 12px;
   text-align: left;
   margin-top: 0;
-}
-.test {
-  margin-top: 40px;
 }
 
 .log-contents {
@@ -161,5 +174,16 @@ export default {
   .level0 {
     color: #000;
   }
+}
+.row2 {
+  margin-top: 10px;
+  box-sizing: content-box;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+
+.el-col {
+  padding: 10px;
 }
 </style>
